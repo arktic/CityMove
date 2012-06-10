bddece64832072806ddad857b663f84184ed92ae
daboooo <julien.uuuu@gmail.com>
2012-06-11 00:33:24 +0200

    conflict

--- Vehicule.java
+++ Vehicule.java
@@ -2,8 +2,7 @@
  *  Classe abstraite,
 <<<<<<< HEAD
  *  permet de regrouper les vehicules
-=======
- *  permet de regrouper les véhicules
+
 >>>>>>> 728b46390fa35a2cf281c6df12e3abbc89cf1fba
  */
 public abstract class Vehicule extends ElementMobiles {
