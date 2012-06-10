bdb7dd932f56839832a0c693e58aeb648830652f
daboooo <julien.uuuu@gmail.com>
2012-06-10 02:05:09 +0200

    voilà 2

---
+++ prg0.c
@@ -0,0 +1,11 @@
+/*============================================================
+    affichage sur la sortie standard de :
+    "UNIX et C c'est dans la poche ..."
+=============================================================*/
+
+#include <stdio.h>                /* definit stdout */
+
+main()
+  {
+  printf("UNIX c'est dans la poche ...\n");
+  }                                
