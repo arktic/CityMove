13246854c69ed5743f089fb70026b5ea00d21bd9
Bilb <audric.bilb@gmail.com>
2012-06-11 00:10:21 +0200

    Ajout de Vehicule

---
+++ Vehicule.java
@@ -0,0 +1,11 @@
+/** 
+ *  Classe abstraite,
+ *  permet de regrouper les véhicules
+ */
+public abstract class Vehicule extends ElementMobiles {
+  /* {author=Groupe X, version=0.1, since=08/06/2012 00/49}*/
+
+
+  public abstract int verifierDeplacement();
+
+}
