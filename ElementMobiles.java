76e4c9802cafd2a1bb6b944857944f1dd34efee9
daboooo <julien.uuuu@gmail.com>
2012-06-17 18:10:09 +0200

    choixDeplacement

--- ElementMobiles.java
+++ ElementMobiles.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public abstract class ElementMobiles extends Elements {
 
 	protected Coordonnee vitesse;
@@ -17,7 +20,7 @@ public abstract class ElementMobiles extends Elements {
 
 
 	/**
-	 * Renvoie la position en TUILES de la prochaine position d'un élément mobile
+	 * Renvoie la position en TUILES de la prochaine position d'un element mobile
 	 * @param dir : la direction courante 
 	 * @return
 	 */
@@ -40,6 +43,29 @@ public abstract class ElementMobiles extends Elements {
 			return new Coordonnee(maMapPosition.getX(), maMapPosition.getY());
 		}
 	}
-
+	
+	/**
+	 * Choisit aleatoirement une direction parmis celles qui lui sont proposees
+	 * @return
+	 */
+	public Direction choixDeplacement() {
+		Direction maDirection = Direction.AUCUNE;
+		
+		/* Je recupere mon mapElement correspondant a mon ElementMobile */
+		Coordonnee posInTiles = Application.map.getPositionInTiles(position);
+		MapElement monMapElement = Application.map.getMapElement(posInTiles);
+		
+		/* Je recupere le tableau de possibilites de ce mapElement puis je stock la taille du tableau */
+		ArrayList<Direction> tab = monMapElement.getPossibilities();
+		int taille = tab.size();
+		
+		/* Je genere un nommbre aleatoire compris entre 0 et la taile de mon tableau -> c'est la que ce fait le choix */
+		Random generator = new Random();
+		int choix = generator.nextInt(taille);
+		
+		maDirection = tab.get(choix);
+		
+		return maDirection;
+	}
 
 }
